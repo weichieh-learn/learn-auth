6bfa40fb4b40aab9cbc8f2c6bf11d148e2652fa1
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2022-01-12 15:00:11 +0800

    show error feedback and loading msg

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef()
 
   const [isLogin, setIsLogin] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState)
@@ -19,6 +20,8 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value
 
     // optional: add validation
+
+    setIsLoading(true)
 
     if (isLogin) {
     } else {
@@ -37,12 +40,16 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false)
         if (res.ok) {
           // ...
         } else {
           return res.json().then((data) => {
-            // show an error modal
-            console.log(data)
+            let errorMessage = 'Authentication failed!'
+            // if(data && data.error && data.error.message) {
+            //   errorMessage = data.error.message
+            // }
+            alert(errorMessage)
           })
         }
       })
@@ -67,7 +74,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p> }
           <button
             type="button"
             className={classes.toggle}
